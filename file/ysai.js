9a357dc07c92c9f8c154ff422c267ce1b411a3db
Perum Loh Agung <61934361+lohagung@users.noreply.github.com>
2025-02-22 02:17:51 +0700

    Update ysai.js

--- file/ysai.js
+++ file/ysai.js
@@ -115,9 +115,11 @@
       document.title = title;
 
       const newItem = document.createElement('div');
+     newItem.innerHTML = `
+        <img alt="${title}" class="w-full h-64 object-cover rounded-lg mb-4" src="${imageUrl}"/>';   
       newItem.className = 'bg-white p-4 rounded-lg shadow w-full max-w-6xl mx-auto';
       newItem.innerHTML = `
-        <img alt="${title}" class="w-full h-64 object-cover rounded-lg mb-4" src="${imageUrl}"/>
+
         <h1 class="text-2xl font-bold mb-2 text-center uppercase">${title}</h1>
         <blockquote class="text-center italic text-gray-600 mb-4">"${snippet}"</blockquote>
         <div class="text-base mb-4">${formatContent(content)}</div>
